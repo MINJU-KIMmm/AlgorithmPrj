c4056b4f632fe31a3615485ddfc277ac01d5e938
MINJU-KIMmm <0112km@ewhain.net>
2021-06-28 22:46:25 +0900

    macTest

---
+++ src/Baekjoon/No10171_고양이.java
@@ -0,0 +1,12 @@
+
+public class No10171_고양이 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|");
+	}
+
+}
