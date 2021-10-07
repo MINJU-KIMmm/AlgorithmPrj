42a5ad6c2d3ed4a6653d9eff735707f506e91f59
MINJU-KIMmm <0112km@ewhain.net>
2021-10-07 17:32:33 +0900

    10/7 - review(다른방법으로 시도)

--- src/BOJ/src/우선순위큐/No11279_최대힙.java
+++ src/BOJ/src/우선순위큐/No11279_최대힙.java
@@ -8,8 +8,7 @@ public class No11279_최대힙 {
 		// TODO Auto-generated method stub
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
-		Queue<Integer> q=new PriorityQueue<>();
-		
+		Queue<Integer> q=new PriorityQueue<>(Comparator.reverseOrder());
 		StringBuilder sb=new StringBuilder();
 		
 		for(int i=0;i<n;i++) {
@@ -18,14 +17,31 @@ public class No11279_최대힙 {
 				if(q.isEmpty()) {
 					sb.append(0+"\n");
 				}else {
-					sb.append(q.poll()*-1+"\n");
+					sb.append(q.poll()+"\n");
 				}
 			}else {
-				//원래는 최소값을 삭제하기 때문에 최댓값을 삭제하기 위해선(최대힙으로 만들기 위해선) -1을 곱해준다 
-				q.add(k*-1);
+				q.add(k);
 			}
 		}
 		
+//		Queue<Integer> q=new PriorityQueue<>();
+//		
+//		StringBuilder sb=new StringBuilder();
+//		
+//		for(int i=0;i<n;i++) {
+//			int k=Integer.parseInt(br.readLine());
+//			if(k==0) {
+//				if(q.isEmpty()) {
+//					sb.append(0+"\n");
+//				}else {
+//					sb.append(q.poll()*-1+"\n");
+//				}
+//			}else {
+//				//원래는 최소값을 삭제하기 때문에 최댓값을 삭제하기 위해선(최대힙으로 만들기 위해선) -1을 곱해준다 
+//				q.add(k*-1);
+//			}
+//		}
+//		
 		System.out.println(sb.toString());
 	}
 
