7b9aea387649f3c825b80f00a3cd97cded1c357d
MINJU-KIMmm <0112km@ewhain.net>
2021-11-08 14:19:52 +0900

    11/8

--- src/BOJ/src/배낭문제/No7579_앱.java
+++ src/BOJ/src/배낭문제/No7579_앱.java
@@ -34,10 +34,10 @@ public class No7579_앱 {
 			int mem = memory[i];
 
 			for (int j = 0; j <= 100000; j++) {
-				if (i == 0) { //첫번째 열인 경우 -> j가 c 이상이면 mem 값을 담는다 
+				if (i == 0) { //첫번째 인 경우 -> j가 c 이상이면 mem 값을 담는다 
 					if (j >= c)
 						dp[i][j] = mem;
-				} else { //첫번째 열이 아닌 경우 
+				} else { //첫번째 행이 아닌 경우 
 					if (j >= c) // j가 c 이상이면 최댓값 계산 ( 1. 이전 행에서 c비용 안쓰고 확보한 메모리 값 + 이번 행에서 c비용으로 확보한 메모리 vs 이전 행에서 같은 비용 써서 확보한 메모리)
 						dp[i][j] = Math.max(dp[i - 1][j - c] + mem, dp[i - 1][j]);
 					else //j가 c 이상이 아니라면 이전 행 
