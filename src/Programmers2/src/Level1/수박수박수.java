e607c9b120be423b8d01592826cec5415b6cf374
MINJU-KIMmm <0112km@ewhain.net>
2021-11-12 19:54:46 +0900

    11/12

---
+++ src/Programmers2/src/Level1/수박수박수.java
@@ -0,0 +1,25 @@
+package Level1;
+
+public class 수박수박수 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String solution(int n) {
+		String answer="";
+		StringBuilder sb=new StringBuilder();
+		
+		for(int i=1;i<=n;i++) {
+			if(i%2!=0) {
+				sb.append("수");
+			}else{
+				sb.append("박");
+			}
+		}
+		
+		answer=sb.toString();
+		return answer;
+	}
+}
