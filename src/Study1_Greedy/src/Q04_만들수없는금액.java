f34a8f58eebc1126ce45a5c80a82eba6c8722942
MINJU-KIMmm <0112km@ewhain.net>
2022-01-09 22:42:32 +0900

    1/9

---
+++ src/Study1_Greedy/src/Q04_만들수없는금액.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+public class Q04_만들수없는금액 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(br.readLine());
+		
+		int[] arr=new int[N];
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int goal=1; //있는지 없는지 체크할 값 
+		for(int coin:arr) {
+			if(goal<coin) break; //없으면 break (만들 수 없는 최솟값을 구하는 문제이므로 coin보다 작을때 break한다)
+			goal+=coin; //만들 수 있으면 coin을 더해 goal 갱신 => 새로운 goal 에 대해 체크  
+		}
+		
+		System.out.println(goal);
+	}
+
+}
---
+++ src/Study1_Greedy/src/Q05_볼링공고르기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+public class Q05_볼링공고르기 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int N=Integer.parseInt(st.nextToken());
+		int M=Integer.parseInt(st.nextToken());
+		
+		int[] arr=new int[N];
+		st=new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		int count=0;
+		
+		for(int i=0;i<N-1;i++) {
+			if(arr[i]==M) break;
+			for(int j=i+1;j<N;j++) {
+				if(arr[i]!=arr[j]) count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
+
+}
