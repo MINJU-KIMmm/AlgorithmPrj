b6d64ac3ee8d7cb4f3cec0c0afeac6c708c56f68
MINJU-KIMmm <0112km@ewhain.net>
2022-01-16 23:37:45 +0900

    1/16

--- src/Study/src/Ch2_Implementation/Q09_김민주.java
+++ src/Study/src/Ch2_Implementation/Q09_김민주.java
@@ -5,20 +5,46 @@ public class Q09_김민주 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		
+		System.out.println(solution(str));
 	}
 
-	public int solution(String s) {
+	static int solution(String s) {
         int answer = 0;
+        ArrayList<String> split=new ArrayList<>();
         ArrayList<Integer> lens=new ArrayList<>();
         
-        for(int i=1;i<=s.length();i++) {
-        	if(!check(i, s.length())) continue;
+        for(int i=1;i<=s.length()/2;i++) {        	
+        	int count=1;
+        	String current=s.substring(0,i);
+        	StringBuilder sb=new StringBuilder();
         	
-        	for(int j=0;j<s.length();j+=i) {
+        	for(int j=i;j<=s.length();j+=i) {
+        		String next="";
+        		if(j+i>s.length()) {
+        			next=s.substring(j, s.length());
+        		}else {
+        			next=s.substring(j, j+i);
+        		}
         		
+        		if(current.equals(next)) count++;
+        		else {
+        			if(count!=1) sb.append(count);
+        			sb.append(current);
+        			current=next;
+        			count=1;
+        		}
         	}
+        	
+        	sb.append(current);
+        	lens.add(sb.toString().length());
         }
+        
+        Collections.sort(lens);
+
+        answer=lens.get(0);
         return answer;
     }
 	
