2c0e232243ac6a67addde3b91709263154322313
MINJU-KIMmm <0112km@ewhain.net>
2022-01-20 23:41:18 +0900

    1/20

--- src/Study/src/Ch2_Implementation/Q10_김민주.java
+++ src/Study/src/Ch2_Implementation/Q10_김민주.java
@@ -19,88 +19,75 @@ public class Q10_김민주 {
 	}
 	
 	public static boolean solution(int[][] key, int[][] lock) {
-        boolean answer = true;
-        int len=lock.length;
-        int[][] checkLock=new int[3*len-2][3*len-2];
-        
-        
-        for(int i=0;i<len;i++) {
-        	for(int j=0;j<len;j++) {
-        		checkLock[len-1+i][len-1+j]=lock[i][j];
-        	}
-        }
-        
-        for(int i=0;i<4;i++) {
-        	key=rotate(key);
-        	
-//        	for(int ky=0;ky<key.length;ky++) {
-//        		for(int jy=0;jy<key.length;jy++) {
-//        			System.out.print(key[ky][jy]);
-//        		}
-//        		System.out.println();
-//        	}
-//        	
-//        	System.out.println();
-        	
-        	for(int j=len-key.length;j<2*len-2;j++) {
-        		for(int k=len-key.length;k<2*len-2;k++) {
-        			isValid(key, checkLock, j, k, len);
-        		}
-        	}
-        	
-        }
-        
-        
-        return answer;
-    }
+		int n = lock.length;
+		int m = key.length;
 
-	
-	//시계방향 90도 
-	static int[][] rotate(int[][] key) {
-		int[][] newKey=new int[key.length][key.length];
-		for(int i=0;i<key.length;i++) {
-			for(int j=0;j<key.length;j++) {
-				newKey[j][key.length-1-i]=key[i][j];
-			}
+		int map[][] = new int[n + 2*(m-1)][n + 2*(m-1)];
+		int temp[][] = new int[map.length][map.length];
+
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) 
+				map[m-1+i][m-1+j] = lock[i][j];
 		}
-		
-		return newKey;
-	}
-	
-	static boolean isValid(int[][] key, int[][] checkLock, int x, int y, int len) {
-		boolean flag=true;
-		for(int i=0;i<key.length;i++) {
-			for(int j=0;j<key.length;j++) {
-				checkLock[x+i][y+j]+=key[i][j];
-			}
-		}
-		
-//		for(int i=0;i<checkLock.length;i++) {
-//			for(int j=0;j<checkLock.length;j++) {
-//				System.out.print(checkLock[i][j]);
-//			}
-//			
-//			System.out.println();
-//		}
-//		
-//		System.out.println();
-		for(int i=len-1;i<2*len-1;i++) {
-			for(int j=len-1;j<2*len-1;j++) {
-				if(checkLock[i][j]!=1) {
-					flag=false;
+
+		for(int i = 0; i <= map.length-m; i++) {
+			for(int j = 0; j <= map.length-m; j++) {
+				l:	for(int k = 0; k < 4; k++) {
+					//key 회전
+					key = rotate(key);
+					//temp 세탁
+					temp = setMap(map);
+
+					//자물쇠에 키를 꽂음
+					for(int x = 0; x < m; x++) {
+						for(int y = 0; y < m; y++) {
+							if(temp[i+x][j+y] == 1 && key[x][y] == 1)
+								continue l;
+							else if(temp[i+x][j+y] == 0 && key[x][y] == 1)
+								temp[i+x][j+y] = 1;
+						}
+					}
+					//자물쇠 검사
+					if(check(temp, n, m))
+						return true;
 				}
 			}
 		}
-		
-		if(!flag) {
-			for(int i=0;i<key.length;i++) {
-				for(int j=0;j<key.length;j++) {
-					checkLock[x+i][y+j]-=key[i][j];
-				}
+
+		return false;
+	}
+
+	public static int[][] rotate(int key[][]) {
+		int len = key.length;
+		int convert[][] = new int[len][len];
+
+		for(int i = 0; i < len; i++) {
+			for(int j = 0; j < len; j++) {
+				convert[j][len-i-1] = key[i][j];
 			}
 		}
-		
-		return flag;
+
+		return convert;
+	}
+
+	public static int[][] setMap(int map[][]) {
+		int len = map.length;
+		int convert[][] = new int[len][len];
+
+		for(int i = 0; i < len; i++) 
+			convert[i] = map[i].clone();
+
+		return convert;
 	}
 	
+	public static boolean check(int map[][], int n, int m) {
+		for(int i = m-1; i < n+m-1; i++) {
+			for(int j = m-1; j < n+m-1; j++)
+				if(map[i][j] == 0)
+					return false;
+		}
+		
+		return true;
+	}
+
 }
